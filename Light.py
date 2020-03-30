ed61f9f2a09dd1cc7bcb3dd9166d0ab689446b73
Hoeun Sim <h.sim312@gmail.com>
2020-03-29 19:12:26 -0700

    Add Light class

---
+++ Light.py
@@ -0,0 +1,18 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+class Light():
+    def __init__(self, id=None):
+        self.id = id
+
+        self.URL = "https://192.168.1.51/api/is83Oh2YZuNuD4Nqoks0nLmfQw32UaGdvn5hQX9n"
+
+        self.r = requests.get(self.URL, verify=False)
+
+        self.data = self.r.json()
+
+        self.lights = self.data['lights']
+
+    def getAllLights(self):
+        return [light for light in self.lights]
