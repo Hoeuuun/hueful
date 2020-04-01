761556b59d471e2f2f0b4e4f0f2fb05da454e9a8
Hoeun Sim <h.sim312@gmail.com>
2020-03-31 19:07:41 -0700

    Implement set state methods for bri, hue, sat, xy, ct

--- Group.py
+++ Group.py
@@ -1,7 +1,7 @@
 import requests
 import logging
 import json
-from typing import Iterable, Dict, Optional
+from typing import Iterable, Dict, Optional, List
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # needed to disable SSL check
 
@@ -16,6 +16,7 @@ class Group():
         self.id = id
         self.URL = "https://192.168.1.51/api/is83Oh2YZuNuD4Nqoks0nLmfQw32UaGdvn5hQX9n/groups"
         self._refresh_state()
+        self.state_URL = self.URL + f"/{id}/action"
 
     def _refresh_state(self) -> Dict:
         '''
@@ -23,14 +24,13 @@ class Group():
         '''
         try:
             response = requests.get(self.URL, verify=False)
-            _logger.info(response.json())
+            # _logger.info(response.json())
             self.data = response.json()
             self.ids = [id for id in self.data]
             return self.data
         except requests.RequestException as e:
             _logger.error("can't get state from HUE bridge.")
             raise e
-
 
     def getAllGroups(self) -> Iterable[str]:
         '''
@@ -56,15 +56,101 @@ class Group():
         _logger.info(f"Cannot find {room} lights!")
         return None
 
+    def getLights(self):
+        '''
+        Returns a list of all light IDs in the system.
+        All lights in the system are part of the immutable group 0.
+        '''
+        URL = self.URL + "/lights"
+        try:
+            resp = requests.get(URL, verify=False)
+            _logger.info(resp.json())
+            data = resp.json()
+            lights = data['lights']
+            return lights
+        except requests.RequestException as e:
+            _logger.error("can't get lights from HUE bridge.")
+            raise e
+
+    def getState(self, id:str):
+        '''
+        Returns a dictionary of the group's current state
+        '''
+        URL = self.URL + f"/{id}"
+        try:
+            resp = requests.get(URL, verify=False)
+            # _logger.info(resp.json())
+            data = resp.json()
+            state = data['action']
+            return state
+        except requests.RequestException as e:
+            _logger.error("can't get state from HUE bridge.")
+            raise e
+
+
+    '''
+    Methods to modify the group's state:
+    '''
     def turnLights(self, id:str, on:bool):
         '''
-        Turns the group's lights on and off
+        Turns the lights on/off
         '''
-        URL = f"https://192.168.1.51/api/is83Oh2YZuNuD4Nqoks0nLmfQw32UaGdvn5hQX9n/groups/{id}/action"
         try:
             body = {"on": on}
-            resp = requests.put(URL, data=json.dumps(body), verify=False)
+            resp = requests.put(self.state_URL, data=json.dumps(body), verify=False)
             _logger.info(resp.json())
             #  _logger.info(resp.content)
         except requests.RequestException as e:
             _logger.error(f"Couldn't communicate with the server. ({e})")
+
+    def setBri(self, id:str, bri:int):
+        '''
+        Sets the brightness, 0 - 254
+        :throws: AssertionError if brightness not within 0-254 in_range
+                 RquestException if calling HUE Server did not succeed.
+        '''
+        assert bri >= 0 and bri <= 254
+        body = {"bri": bri}
+        resp = requests.put(self.state_URL, data=json.dumps(body), verify=False)
+        _logger.info(resp.json())
+
+    def setHue(self, id:str, hue:int):
+        '''
+        Sets the hue, 0 - 65535
+        Red = 0, 65535
+        Green = 25500
+        Blue = 46920
+        '''
+        assert hue >= 0 and hue <= 65535
+        body = {"hue": hue}
+        resp = requests.put(self.state_URL, data=json.dumps(body), verify=False)
+        _logger.info(resp.json())
+
+    def setSat(self, id:str, sat:int):
+        '''
+        Sets the staturation, 0 - 254
+        '''
+        assert sat >= 0 and sat <= 254
+        body = {"sat": sat}
+        resp = requests.put(self.state_URL, data=json.dumps(body), verify=False)
+        _logger.info(resp.json())
+
+    def setXY(self, id:str, xy:List[float]):
+        '''
+        Sets the X and Y coordinates of a color in the CIE color space, 0.0 - 1.0
+        '''
+        assert xy[0] >= 0 and xy[1] <= 1 and xy[1] >= 0 and xy[1] <= 1
+        body = {"xy": [xy[0], xy[1]]}
+        resp = requests.put(self.state_URL, data=json.dumps(body), verify=False)
+        _logger.info(resp.json())
+
+    def setCT(self, id:str, ct:int):
+        '''
+        Sets the Mired Color temperature, 153 (6500K) - 500 (2000K)
+        '''
+        assert ct >= 153 and ct <= 500
+        body = {"ct": ct}
+        resp = requests.put(self.state_URL, data=json.dumps(body), verify=False)
+        _logger.info(resp.json())
+
+    # TODO: alert, effect, transitiontime, increments for each, scene
--- test.py
+++ test.py
@@ -2,21 +2,31 @@ from Group import Group
 from Light import Light
 
 g = Group()
-print(g.getAllGroups())
-# ['1', '2', '3', '4', '5', '6', '8']
+# print(g.getAllGroups())
+# # ['1', '2', '3', '4', '5', '6', '8']
+#
+# print(g.getGroup('1'))
+#
+# print(g.getGroupByRoom("Living room"))
+#
+# print(g.getGroupByRoom("Dining room"))
+# # Cannot find Dining room lights!
+# # None
+#
+# l = Light()
+# print(l.getAllLights())
+# # ['2', '4', '5', '6', '7', '8']
+#
+# print("Turning lights")
+# g.turnLights('1', True)
+# #g.turnLights('1', False)
 
-print(g.getGroup('1'))
 
-print(g.getGroupByRoom("Living room"))
-
-print(g.getGroupByRoom("Dining room"))
-# Cannot find Dining room lights!
-# None
-
-l = Light()
-print(l.getAllLights())
-# ['2', '4', '5', '6', '7', '8']
-
-print("Turning lights")
-g.turnLights('1', True)
-#g.turnLights('1', False)
+# print(g.getLights())
+print("----")
+print(g.getState(1))
+g.setBri(1,200)
+g.setHue(1,25500)
+g.setSat(1, 100)
+g.setXY(1, [0.555, 0.92])
+g.setCT(1, 500)
